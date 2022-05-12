e99c10412a8d8968cae3bd08223c166cf1f9974d
Sevdanur GENC <sevdanurgenc@gmail.com>
2022-05-12 08:32:01 +0300

    Add files via upload

---
+++ CSharp-Design-Patterns/SingletonDesignPattern/SingletonDesignPattern/SingletonDesignPattern/DBHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDesignPattern.SingletonDesignPattern
+{
+    public class DBHelper
+    {
+        private DBHelper(string connectionString)
+        {
+            this.ConnectionString = connectionString;
+        }
+
+        public string ConnectionString { get; set; }
+               
+        private static DBHelper _helper;
+
+        public static DBHelper CreateDBHelper(string connectionString)
+        {
+            if (_helper == null)
+            {
+                _helper = new DBHelper(connectionString);
+            }
+            return _helper;
+        }
+
+        public string StateMessage { get; set; }
+
+    }
+}
